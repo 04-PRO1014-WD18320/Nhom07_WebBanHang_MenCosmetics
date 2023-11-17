1252fd5e85ead7534379a3272e0dfe2a994a52b8
Kieu Van Quyet <kieuvanquyet30122003@gmail.com>
2023-11-17 22:05:19 +0700

    ngay 1

---
+++ admin/footer.php
@@ -0,0 +1,9 @@
+<footer>&copy; 2023 Bảng điều khiển Admin</footer>
+  </body>
+  <script>
+    function toggleSubMenu(element) {
+      var subMenu = element.querySelector(".sub-menu");
+      subMenu.classList.toggle("active");
+    }
+  </script>
+</html>
