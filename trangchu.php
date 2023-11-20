4a970cd9e83cb8746106dba346113851ed74ed3e
Duyhk711 <duyhkph41474@fpt.edu.vn>
2023-11-20 22:41:24 +0700

    toi 20/11

---
+++ trangchu.php
@@ -0,0 +1,84 @@
+<!-- begin main -->
+<section class="banner">
+        <img src="img/anh1.jpg" alt="Banner Image">
+    </section>
+
+    <section class="products">
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            <p class="product-price">$99.99</p>
+            <button class="add-to-cart">Add to Cart</button>
+        </div>
+
+        <!-- Add more product elements here -->
+    </section>
+    
+    <div class="h2"><h2>DANH MỤC SẢN PHẨM</h2></div>
+    <div class="danhmuc">
+        
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+           
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+           
+        </div>
+        <div class="product">
+            <img src="img/sap1.jpg" alt="Product 1">
+            <p class="product-description">Product Description</p>
+            
+        </div>
+    </div>
+
+    <!-- end main -->
---
+++ view/chitietsanpham.php
@@ -0,0 +1,83 @@
+<main class="main-ctsp">
+        <div class="container">
+            <div class="pro">
+                <img src="./img/sap1.jpg" alt="">
+                <div class="detail">
+                    <div class="name">
+                        <h5>SÁP VUỐT TÓC</h5>
+                        <div class="price">500.000 VNĐ</div>
+                        <div class="quality">
+                            <p>Số lượng</p>
+                            <input type="number" name="" value="1" min="1"></div>
+                        <div class="action">
+                            <input type="button" value="Thêm Giỏ Hàng">
+                            <input type="button" value="Mua Ngay">
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="description">
+                <h2>Mô Tả</h2>
+                <p>Giới thiệu sản phẩm</p>
+                <img src="./img/sap1.jpg">
+                <p>Thông tin chi tiết</p>
+                <p>Hướng dẫn sử dụng</p>
+            </div>
+
+            <h2>Bình luận</h2>
+            
+            <div class="comment">
+                
+                <table>
+                    <tr>
+                        <th>Tài Khoản</th>
+                        <th>Nội Dung</th>
+                        <th>Ngày Bình Luận</th>
+                    </tr>
+    
+                    <tr>
+                        <td>huong</td>
+                        <td>đây là bình luận</td>
+                        <td>18/11/2023</td>
+                    </tr>
+    
+                    <tr>
+                        <td>trang</td>
+                        <td>đây là bình luận</td>
+                        <td>18/11/2023</td>
+                    </tr>
+                </table>
+            </div>
+            <h2>Sản Phẩm Tương Tự</h2>
+            <div class="samePro">
+                
+                <section class="products">
+                    <div class="product">
+                        <img src="img/sap1.jpg" alt="Product 1">
+                        <p class="product-description">Product Description</p>
+                        <p class="product-price">$99.99</p>
+                        <button class="add-to-cart">Add to Cart</button>
+                    </div>
+                    <div class="product">
+                        <img src="img/sap1.jpg" alt="Product 1">
+                        <p class="product-description">Product Description</p>
+                        <p class="product-price">$99.99</p>
+                        <button class="add-to-cart">Add to Cart</button>
+                    </div>
+                    <div class="product">
+                        <img src="img/sap1.jpg" alt="Product 1">
+                        <p class="product-description">Product Description</p>
+                        <p class="product-price">$99.99</p>
+                        <button class="add-to-cart">Add to Cart</button>
+                    </div>
+                    <div class="product">
+                        <img src="img/sap1.jpg" alt="Product 1">
+                        <p class="product-description">Product Description</p>
+                        <p class="product-price">$99.99</p>
+                        <button class="add-to-cart">Add to Cart</button>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </main>
